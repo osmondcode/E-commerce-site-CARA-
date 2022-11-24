13c4ef956048d0972f084185f1e8bdac771864fa
Osmond <105639028+osmondcode@users.noreply.github.com>
2022-11-24 09:03:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-var bar = document.getElementsByClassName('bars')
+// Single Product Slider
+var mainImg = document.getElementById('main-img');
+var smallImg = document.getElementsByClassName('small-img');
+
+smallImg[0].onclick = function(){
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+    mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+    mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function(){
+    mainImg.src = smallImg[3].src;
+}
